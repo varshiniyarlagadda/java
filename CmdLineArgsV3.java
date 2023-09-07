d9abd1a3bf0028de343bb8922044902d1ecf3053
varshiniyarlagadda <143867571+varshiniyarlagadda@users.noreply.github.com>
2023-09-07 16:06:22 +0530

    Create CmdLineArgsV3.java

---
+++ CmdLineArgsV3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class CmdLineArgsV3 extends Object {
+    public static void main(String[] args){
+        CmdLineArgsV3 obj=new CmdLineArgsV3();
+        if(args.length!=2){
+            System.out.println("Invalid number of arguments \n you need to pass two  integers ");
+        }
+        else{
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int z = x+y;
+        System.out.println("The class name is :- "+obj.getClass());
+        System.out.println("Sum of "+x+" and "+y+ " is "+z);
+        }
+}
+}
