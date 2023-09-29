79e7f51f9c0be3ca9be4ed17e2a3394a9a3f34fe
varshiniyarlagadda <143867571+varshiniyarlagadda@users.noreply.github.com>
2023-09-29 12:08:22 +0530

    Create StaticBlockDemo.java

---
+++ StaticBlockDemo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+ public class StaticBlockDemo{
+     static{
+         System.out.println("Inside static block ");
+     }
+ 
+     public static void main(String[] args){
+         System.out.println("Static block Demo");
+     }
+ }
