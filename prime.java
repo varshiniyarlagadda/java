5927323f28d965bf0fb355d8f0f0c2baf2459aca
varshiniyarlagadda <143867571+varshiniyarlagadda@users.noreply.github.com>
2023-09-30 11:22:03 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class Prime {
+
+    public static void main(String[] args) {
+
+        int low = 23, high = 97;
+
+        while (low <= high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && low != 0 && low != 1)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
